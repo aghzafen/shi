3d854a0b70c6d72a8d547c2c948f53a82f779d61
Souhail Aghzafen <s.aghzafen@gmail.com>
2020-04-21 00:38:55 +0200

    initial commit
    Signed-off-by: Souhail Aghzafen <s.aghzafen@gmail.com>

---
+++ source/main/Main.cpp
@@ -0,0 +1,35 @@
+// vc17.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "SDL.h"
+
+int main(int argc, char* argv[])
+{
+    std::cout << "Hello World!\n";
+	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_Window* window = SDL_CreateWindow("title", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+		600, 400, SDL_WINDOW_SHOWN);
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+
+	SDL_SetRenderDrawColor(renderer, 255, 255, 0, 0);
+	
+	SDL_RenderClear(renderer);
+
+	SDL_RenderPresent(renderer);
+
+	SDL_Delay(5000);
+
+	return EXIT_SUCCESS;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
